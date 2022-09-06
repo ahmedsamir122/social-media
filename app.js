82616b19cd1e38dbf2a7be4cc7cb7bf6a8911777
ahmedsamir122 <eng.ahmed.elmitwalli@gmail.com>
2022-09-06 07:00:05 +0200

    finish the site

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const tools = document.querySelectorAll('.toolbar');
 const noteNum = document.querySelector('#noteNum');
 const msgNum = document.querySelector('#msgNum');
 const noteContent = document.querySelector('.note-content');
+const msgContainer = document.querySelector('.messages');
 const inputMsg = document.querySelector('.messages-input input');
 const msgRows = document.querySelectorAll('.messages-in-name');
 const inputNames = document.querySelectorAll('.messages-in h3');
@@ -11,6 +12,10 @@ const setting = document.querySelector('.setting');
 const settingFont = document.querySelector('.setting-font');
 const settingColors = document.querySelector('.setting-colors');
 const settingBackground = document.querySelector('.setting-background');
+const btnPost = document.querySelector('.left button');
+const headerBtn = document.querySelector('input[type="submit"]');
+const headerInput = document.querySelector('input[type="text"]');
+
 
 console.log(inputNames);
 inputNames.forEach(name => {
@@ -73,10 +78,10 @@ settingFont.addEventListener('click', (e) => {
     document.querySelector('body').style.fontSize='13px';
     if(e.target.classList.contains('font--16'))
     document.querySelector('body').style.fontSize='16px';
-    if(e.target.classList.contains('font--19'))
-    document.querySelector('body').style.fontSize='19px';
-    if(e.target.classList.contains('font--22'))
-    document.querySelector('body').style.fontSize='22px';
+    if(e.target.classList.contains('font--17'))
+    document.querySelector('body').style.fontSize='17px';
+    if(e.target.classList.contains('font--18'))
+    document.querySelector('body').style.fontSize='18px';
 })
 settingColors.addEventListener('click', (e) => {
     const click = e.target.closest('span');
@@ -103,4 +108,29 @@ settingBackground.addEventListener('click', (e) => {
         span.classList.remove('active');
     })
     click.classList.add('active');
+    if(click.classList.contains('bg-white')){
+        document.documentElement.style.setProperty('--bg-main','rgb(243, 241, 241)');
+        document.documentElement.style.setProperty('--box-color','white');
+    }
+    if(click.classList.contains('bg-blue')){
+        document.documentElement.style.setProperty('--bg-main','rgb(9, 9, 51)');
+        document.documentElement.style.setProperty('--box-color','rgba(0, 0, 255, 0.555)');
+    }
+    
+    if(click.classList.contains('bg-dark')){
+        document.documentElement.style.setProperty('--bg-main','black');
+        document.documentElement.style.setProperty('--box-color','rgba(0, 0, 255, 0.555)');
+    }
+})
+
+btnPost.addEventListener('click', (e) => {
+    msgContainer.classList.add('active');
+    setTimeout(() => {
+        msgContainer.classList.remove('active');
+    },2000)   
+})
+
+headerBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    headerInput.focus();
 })
